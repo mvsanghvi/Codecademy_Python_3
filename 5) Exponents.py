fc0f7d504e8217b2c07ccbedb0871b9cb4d5f03c
mvsanghvi <mevsanghvi@gmail.com>
2023-12-05 17:15:15 -0600

    New Practice

---
+++ 5) Exponents.py
@@ -0,0 +1,25 @@
+# Python can also perform exponentiation. In written math, you might see an exponent as a superscript number, but typing superscript numbers isn’t always easy on modern keyboards. Since this operation is so related to multiplication, we use the notation **.
+
+#     # 2 to the 10th power, or 1024
+#     print(2 ** 10)
+
+#     # 8 squared, or 64
+#     print(8 ** 2)
+
+#     # 9 * 9 * 9, 9 cubed, or 729
+#     print(9 ** 3)
+
+#     # We can even perform fractional exponents
+#     # 4 to the half power, or 2
+#     print(4 ** 0.5)
+
+# Here, we compute some simple exponents. We calculate 2 to the 10th power, 8 to the 2nd power, 9 to the 3rd power, and 4 to the 0.5th power.
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2)
+# 7x7 quilt
+print(7 ** 2)
+# 8x8 quilt
+print(8 ** 2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 ** 4)
