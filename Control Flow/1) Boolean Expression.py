85b793fe38bb042c3a1c8c74f68608f54770fc71
mvsanghvi <mevsanghvi@gmail.com>
2025-01-04 14:02:58 -0600

    Edit

--- Control Flow/1) Boolean Expression.py
+++ Control Flow/1) Boolean Expression.py
@@ -1,3 +1,35 @@
+#To build control flow in our program, we need to be able to check whether a condition is true or not. 
+# For this, we use Boolean expressions. # A Boolean expression is a statement that can either be true or false. 
+# The statement must be answered by true or false only, and it must be verifiable with evidence. 
+# It cannot rely on interpretation or opinion.
+# To understand this, let’s go back to the ‘waking up’ example. 
+# The first question, “Is today a weekday?” can be written as a Boolean expression shown below: 
+"Today is a weekday."
+#This expression can be true - if today is Tuesday, for example - or false if it’s a weekend. 
+# There are no other options, we know the answer is verifiable, and it does not rely on an opinion. # Now, consider the following phrase:
+"Friday is the best day of the week."
+#  Is this a Boolean expression? No, this statement is an opinion and is subjective. 
+# Someone might say that “Wednesday is the best day.” and their statement would be no less true or false than the one above. 
+# How about the following phrase: 
+"Sunday starts with the letter 'C.'"
+#Yes, this is a Boolean expression since the answer can only be true or false and it is verifiable. 
+# Even though the statement itself is false (Sunday starts with the letter ‘S’ and not ‘C’), it is still a Boolean expression.
+# 1. Examine the following statement.
+
+"The Earth revolves around the Sun."
+
+# If you think it’s a Boolean statement, set the variable first_statement to "Yes". Else, set it to "No".  
 first_statement = "Yes"
+#2. Now, consider the following statement.
+
+"The Moon is made of cheese."
+
+#If you think it’s a Boolean statement, create a variable second_statement and set it to "Yes". Otherwise, set it to "No". 
 second_statement = "Yes"
+#3. Next, consider the following statement.
+
+"Chocolate ice cream tastes better than vanilla."
+
+#If the statement qualifies as a Boolean expression, initialize a variable third_statement to "Yes". 
+# If it doesn’t, initialize the variable third_statement to "No". 
 third_statement = "No"
--- Control Flow/2) Relational Operators.py
+++ Control Flow/2) Relational Operators.py
@@ -1,3 +1,27 @@
+#Now that we understand Boolean expressions, let’s learn how to create them. In Python, we create Boolean expressions using relational and logical operators.
+# -Relational operators compare two values and return True or False based on the operands. For this reason, you will sometimes hear them called comparison operators.
+# -Logical operators are used to combine multiple Boolean expressions.
+#The two relational operators we’ll cover first are equals to and not equals to.
+# -The equals to operator (==) returns True if both its operands are the same. Otherwise, it returns False. For example, the Boolean expression a == b will return True if the values a and b are the same. Otherwise, it will return False.
+# -The not equals to operator (!=) is the negation of the equals to operator. The Boolean expression a != b will return True if the values a and b are different. Otherwise, it will return False.
+# We can create Boolean expressions by comparing two values using these operators as shown below: 
+1 == 1     # Evaluates to True as the operands are the same 
+
+1 != 1     # Evaluates to False as the operands are the same 
+
+2 != 4     # Evaluates to True as the operands are different 
+
+3 == 5     # Evaluates to False as the operands are different
+ 
+'7' == 7   # Evaluates to False as the operands are different types 
+#1. Determine if the following Boolean expression is True or False. Assign your answer as True or False in the variable first_expression.
+2 * 2 == 2 + 2
 first_expression = True
+#2. Next, consider the following expression:
+3 + 3 != 3 * 3 
+#If the above Boolean expression is True, initialize a variable second_expression to True. Otherwise, initialize it to False. 
 second_expression = True
+#3. Now, consider this expression:
+3 * 3 == '9'
+#If the above Boolean expression is True, initialize a variable third_expression to True. Otherwise, initialize it to False.
 third_expression = False
