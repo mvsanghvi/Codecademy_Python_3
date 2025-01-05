b7217cf2e32911a6def6ab1ce3558e0ad9af40b3
mvsanghvi <mevsanghvi@gmail.com>
2025-01-05 16:44:20 -0600

    New Exercise

---
+++ Lists/1) What is a list.py
@@ -0,0 +1,13 @@
+# 1. Examine the existing list heights in your code editor. A new student just joined the class!
+
+#     Chloe is 65 inches tall
+
+# Add Chloe’s height to the end of the list heights
+#heights = [61, 70, 67, 64]
+heights = [61, 70, 67, 64, 65]
+
+#2. Remove the # in front of the definition of the list broken_heights. If you run this code, you’ll get an error in your terminal:
+#SyntaxError: invalid syntax
+#Add commas (,) to broken_heights so that it runs without errors.
+#broken_heights = [65 71 59 62]
+broken_heights = [65, 71, 59, 62]
