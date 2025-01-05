dec9e1dca83a5cd66b7e17c34362790a5596152e
mvsanghvi <mevsanghvi@gmail.com>
2025-01-05 16:56:01 -0600

    New Exercise

---
+++ Lists/5) Growing a List_Append.py
@@ -0,0 +1,10 @@
+# 1. Jiho works for a gardening store called Petal Power. Jiho keeps a record of orders in a list called orders.
+# Use print to inspect the orders he has received today.
+orders = ["daisies", "periwinkle"]
+print(orders)
+# 2. Jiho just received a new order for "tulips". Use append to add this string to orders.
+orders.append("tulips")
+# 3. Another order has come in! Use append to add "roses" to orders.
+orders.append("roses")
+#4. Use print to inspect the orders Jiho has received today.
+print(orders)
