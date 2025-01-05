632933188397430ec2c8bcb471f042573cae1071
mvsanghvi <mevsanghvi@gmail.com>
2025-01-05 17:15:28 -0600

    New Exercise

---
+++ Lists/7) Accessing List Elements.py
@@ -0,0 +1,8 @@
+#1. Use square brackets ([ and ]) to access the 4th employee from the list employees. Save it to the variable employee_four.
+employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
+employee_four = employees[3]
+#2. Paste the following code into script.py:
+print(employees[8])
+# 3. # Accessing an element that does not exist produces an IndexError.
+# In the line of code that you pasted, change 8 to an index that exists so that you don’t get an IndexError. Run your code again!
+print(employees[6])
