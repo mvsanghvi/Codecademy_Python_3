a68b23ba42b580db24390fce446958cd0b8410c6
mvsanghvi <mevsanghvi@gmail.com>
2025-01-06 14:18:45 -0600

    New Exercise

---
+++ Lists/22) Counting in a list.py
@@ -0,0 +1,8 @@
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", 
+         "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+# 1. Mrs. Wilson’s class is voting for class president. She has saved each student’s vote into the list votes.
+# Use .count() to determine how many students voted for "Jake" and save the value to a variable called jake_votes.
+jake_votes = votes.count("Jake")
+
+# 2. Use print() to examine jake_votes
+print(jake_votes)
