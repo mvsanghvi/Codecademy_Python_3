cc43a9ef8a43b87ef43e14bb38fa10474f58b618
mvsanghvi <mevsanghvi@gmail.com>
2025-01-06 14:31:28 -0600

    New Exercise

---
+++ Lists/24) Sorting Lists II.py
@@ -0,0 +1,8 @@
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+# 1. Use sorted() to order games and create a new list called games_sorted.
+games_sorted = sorted(games)
+
+# 2. Print both games and games_sorted. How are they different?
+print(games)
+print(games_sorted)
