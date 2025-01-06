445a250794657ae7571b8fe58be26c960a1d8a7b
mvsanghvi <mevsanghvi@gmail.com>
2025-01-06 14:11:44 -0600

    New Exercise

---
+++ Lists/20) Slicing Lists I.py
@@ -0,0 +1,15 @@
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:4]
+# 1. Use print() to examine the variable beginning. Before hitting Run think about what elements beginning will contain?
+print(beginning)
+
+# 2. Modify beginning, so that it selects the first 2 elements of suitcase.
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:2]
+
+# 3. Create a new list called middle that contains the middle two items ( ["pants", "pants"] ) from suitcase.
+# Print middle to see the slice!
+middle = ["pants", "pants"]
+print(middle)
