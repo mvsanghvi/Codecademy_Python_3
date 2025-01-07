b3f0817d21babcf32695ce100738fac3be342cfd
mvsanghvi <mevsanghvi@gmail.com>
2025-01-07 12:53:33 -0600

    New Exercise

---
+++ Loops/1) Why Loops.py
@@ -0,0 +1,11 @@
+# 1. Using 10 print() statements, print out: "This can be so much easier with loops!".
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
