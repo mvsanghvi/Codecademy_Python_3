1b622a55c6b88dc12fbf59d9952c1002ab5d7727
mvsanghvi <mevsanghvi@gmail.com>
2025-01-07 13:01:21 -0600

    New Exercise

---
+++ Loops/3) For Loops_Using range.py
@@ -0,0 +1,5 @@
+# 1. Use the range() function in a for loop to print() out the provided promise variable five times.
+promise = "I will finish the python loops module!"
+
+for temp in range(5):
+  print(promise)
